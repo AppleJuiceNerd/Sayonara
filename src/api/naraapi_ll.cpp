501609e01537d609ed5d0e2ebb8656c9bded08ea
AppleJuiceNerd <81881221+AppleJuiceNerd@users.noreply.github.com>
2026-03-19 15:23:30 -0500

    Move LoadBytes

--- src/api/naraapi_ll.cpp
+++ src/api/naraapi_ll.cpp
@@ -112,6 +112,49 @@ void Nara::LL::LightData::GetBytes(uint8_t *bytes)
 	}
 }
 
+
+void Nara::LL::Packet::GetBytes(uint8_t *bytes)
+{
+	int size;
+	
+	// Check if the packet should be long 
+	if (long_packet)
+	{
+		size = 1024;
+		bytes[0] = 0x22;
+	} else
+	{
+		size = 64;
+		bytes[0] = 0x21;
+	}
+	
+	bytes[1] = echo;
+	
+	
+	// setup loop
+	// offset in the packet
+	int offset = 4;
+	
+	// The bytes that will be filled by the packages' GetBytes function
+	
+	uint8_t buffer[1024] = { 0 };
+	
+	for(int pkg = 0; pkg < packages.size(); pkg++)
+	{
+		// Assemble package bytes
+		packages[pkg]->GetBytes(buffer);
+		
+		// Copy package to bytes at offset
+		memcpy(&bytes[offset], &buffer, packages[pkg]->GetLength());
+
+		// Move to the next available space
+		// There should be four zeros after each package
+		offset += packages[pkg]->GetLength() + 4;
+	}
+	
+	*(uint16_t*)(&bytes[2]) = checksum(bytes, size);
+}
+
 void Nara::LL::LightData::LoadBytes(uint8_t *bytes)
 {
 	// Header
@@ -132,49 +175,6 @@ void Nara::LL::LightData::LoadBytes(uint8_t *bytes)
 	{
 		memcpy(&led_fn[i], &bytes[20 + (i * 8)], 8);
 	}
-}
-
-
-void Nara::LL::Packet::GetBytes(uint8_t *bytes)
-{
-	int size;
-
-	// Check if the packet should be long 
-	if (long_packet)
-	{
-		size = 1024;
-		bytes[0] = 0x22;
-	} else
-	{
-		size = 64;
-		bytes[0] = 0x21;
-	}
-
-	bytes[1] = echo;
-
-
-	// setup loop
-	// offset in the packet
-	int offset = 4;
-
-	// The bytes that will be filled by the packages' GetBytes function
-	
-	uint8_t buffer[1024] = { 0 };
-
-	for(int pkg = 0; pkg < packages.size(); pkg++)
-	{
-		// Assemble package bytes
-		packages[pkg]->GetBytes(buffer);
-
-		// Copy package to bytes at offset
-		memcpy(&bytes[offset], &buffer, packages[pkg]->GetLength());
-
-		// Move to the next available space
-		// There should be four zeros after each package
-		offset += packages[pkg]->GetLength() + 4;
-	}
-
-	*(uint16_t*)(&bytes[2]) = checksum(bytes, size);
 }
 
 
